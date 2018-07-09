ac0c23cc58099b7f12b49c73180ec1d3c47f2d6e
SOULARD <valentine.soulard.externe@sogeti.com>
2018-07-09 10:32:09 +0200

    essai_photos_3

--- src/pages/photos/photos.ts
+++ src/pages/photos/photos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PhotoLibrary } from '@ionic-native/photo-library';
+import { Toast } from '@ionic-native/toast';
 
 /**
  * Generated class for the PhotosPage page.
@@ -16,34 +17,39 @@ import { PhotoLibrary } from '@ionic-native/photo-library';
 })
 export class PhotosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private photoLibrary: PhotoLibrary) {
-  
+  albumsBase: string[] = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private photoLibrary: PhotoLibrary, private toast: Toast) {
+
     this.photoLibrary.requestAuthorization().then(() => {
       this.photoLibrary.getLibrary().subscribe({
         next: library => {
-          library.forEach(function(libraryItem) {
-            console.log(libraryItem.id);          // ID of the photo
-            console.log(libraryItem.photoURL);    // Cross-platform access to photo
-            console.log(libraryItem.thumbnailURL);// Cross-platform access to thumbnail
-            console.log(libraryItem.fileName);
-            console.log(libraryItem.width);
-            console.log(libraryItem.height);
-            console.log(libraryItem.creationDate);
-            console.log(libraryItem.latitude);
-            console.log(libraryItem.longitude);
-            console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
+          library.forEach(function (libraryItem) {
+            this.ShowToast(libraryItem.thumbnailURL);
+            this.albumsBase.add(libraryItem.thumbnailURL);
           });
         },
-        error: err => { console.log('could not get photos'); },
-        complete: () => { console.log('done getting photos'); }
+        error: err => { this.ShowToast('could not get photos'); },
+        complete: () => { this.ShowToast('done getting photos'); }
       });
     })
-    .catch(err => console.log('permissions weren\'t granted')); 
-  
+      .catch(err => this.ShowToast('permissions weren\'t granted'));
+
+    
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PhotosPage');
   }
+
+  ShowToast(message:string){
+    this.toast.show(`I'm a toast`, '5000', 'center').subscribe(
+      toast => {
+        console.log(toast);
+      }
+  }
+
+
 
 }
